750b48ab557921b088dc2231d1df619eb211f4c1
rmakurumure <rmakurumure@netone.co.zw>
2024-09-17 14:15:10 +0200

    initial commit

---
+++ src/test/java/com/afrocodemy/demo_springboot/DemoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afrocodemy.demo_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
